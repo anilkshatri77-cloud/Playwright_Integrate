7f9b5f4e5d6991cf11c4a0c13364a7d67350d8ad
Anil Kshatri <anilkshatri77@gmail.com>
2026-04-28 15:49:07 +0530

    Playwright.spec.js

---
+++ tests/Group.spec.js
@@ -0,0 +1,22 @@
+const { test, expect } = require('@playwright/test')
+
+test.describe('Smoke Testing', () => {
+
+    //write a test1
+    test('Test1', async ({ page }) => {
+    })
+    //write a test2
+    test('Test2', async ({ page }) => {
+    })
+});
+test.describe('Sanity Testing', () => {
+    //write a test3
+    test('Test3', async ({ page }) => {
+    })
+    //Write a test4
+    test('Test4', async ({ page }) => {
+    })
+    //Write a test5
+    test('Test5', async ({ page }) => {
+    })
+});

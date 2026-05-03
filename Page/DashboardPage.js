13792fd9d8896d0e35412c8d77b54bbbfcb44bce
Anil Kshatri <anilkshatri77@gmail.com>
2026-05-03 18:08:40 +0530

    Playwright_Test

--- Page/DashboardPage.js
+++ Page/DashboardPage.js
@@ -1,24 +1,24 @@
-exports.DashboardPage = class DashboardPage {
-    constructor(page) {
-        this.page = page;
-        this.header = '.app_logo';
-        this.firstProduct = '.inventory_item:first-child';
-        this.firstProductName = '.inventory_item:first-child .inventory_item_name';
-    }
+// exports.DashboardPage = class DashboardPage {
+//     constructor(page) {
+//         this.page = page;
+//         this.header = '.app_logo';
+//         this.firstProduct = '.inventory_item:first-child';
+//         this.firstProductName = '.inventory_item:first-child .inventory_item_name';
+//     }
 
-    async getHeaderText() {
-        return await this.page.locator(this.header).textContent();
-    }
+//     async getHeaderText() {
+//         return await this.page.locator(this.header).textContent();
+//     }
 
-    async getFirstProductName() {
-        return await this.page.locator(this.firstProductName).textContent();
-    }
+//     async getFirstProductName() {
+//         return await this.page.locator(this.firstProductName).textContent();
+//     }
 
-    async isHeaderVisible() {
-        return await this.page.locator(this.header).isVisible();
-    }
+//     async isHeaderVisible() {
+//         return await this.page.locator(this.header).isVisible();
+//     }
 
-    async isFirstProductVisible() {
-        return await this.page.locator(this.firstProduct).isVisible();
-    }
-};
+//     async isFirstProductVisible() {
+//         return await this.page.locator(this.firstProduct).isVisible();
+//     }
+// // }

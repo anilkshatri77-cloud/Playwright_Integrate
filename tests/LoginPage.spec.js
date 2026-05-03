b3e2f4a54d380ae4befb60773fc6279d068bc1b9
Anil Kshatri <anilkshatri77@gmail.com>
2026-05-03 16:41:47 +0530

    Playwright_Test

---
+++ tests/LoginPage.spec.js
@@ -0,0 +1,15 @@
+const { test, expect } = require("@playwright/test")
+
+test ('Login Demo Test', async({page}) => {
+
+   await page.goto('https://www.saucedemo.com/');
+   await page.getByPlaceholder('Username').fill('standard_user');
+   await page.getByPlaceholder('Password').fill('secret_sauce');
+   await page.getByRole('button', {type : 'submit'}).click();
+
+   
+
+   await page.waitForTimeout(5000);
+
+});
+

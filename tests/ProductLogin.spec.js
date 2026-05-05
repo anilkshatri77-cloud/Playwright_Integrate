a92455098c3a6c3623443964a98ed10cdebffb15
Anil Kshatri <anilkshatri77@gmail.com>
2026-05-05 12:36:02 +0530

    Playwright_Test

--- tests/ProductLogin.spec.js
+++ tests/ProductLogin.spec.js
@@ -4,15 +4,28 @@ test('Login Test', async ({ page }) => {
     await page.goto('https://www.demoblaze.com/');
 
     await expect(page.getByRole('link', { name: 'Sign up' })).toBeVisible();
-    await page.locator('#carouselExampleIndicators').getByRole('button', { name: 'Next' }).click();
     await page.getByRole('link', { name: 'Sign up' }).click();
+
     await expect(page.getByRole('heading', { name: 'Sign up' })).toBeVisible();
 
-    await expect(page.getByLabel('Username:')).toBeVisible();
+    await expect(page.getByLabel('Sign up').getByText('Username:')).toBeVisible();
 
-    //span[normalize-space()='Flights']
+    //User Input the Username field
+    await expect(page.getByRole('textbox', { name: 'Username:' })).toBeVisible();
+    await page.getByRole('textbox', { name: 'Username:' }).click();
+    await page.getByRole('textbox', { name: 'Username:' }).fill('Anil Kshatri');
+    await expect(page.getByRole('textbox', { name: 'Username:' })).toBeVisible();
 
-    await page.getByText('Flights', { exact: true })
+    //User Input the Password field
+    await expect(page.getByLabel('Sign up').getByText('Password:')).toBeVisible();
+    await expect(page.getByRole('textbox', { name: 'Password:' })).toBeVisible();
+    await page.getByRole('textbox', { name: 'Password:' }).click();
+    await page.getByRole('textbox', { name: 'Password:' }).fill('@Anil123');
+    await expect(page.getByRole('textbox', { name: 'Password:' })).toBeVisible();
+
+
     // await page.waitForTimeout(500);
 
+
+    await page.getByRole('link', { name: 'Amazon.in' })
 });
